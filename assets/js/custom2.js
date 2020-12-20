4155b3e2e73a26644dd65dd71e4968cd4ef02547
starahmad6161 <starahmad616161@gmail.com>
2020-12-21 01:20:29 +0200

    fix slider

---
+++ assets/js/custom2.js
@@ -0,0 +1,72 @@
+$(function() {
+    'use strict';
+    $('.recommended-box-slider').owlCarousel({
+	    //loop:true,
+	    margin:25,
+	    dots: true,
+	    nav:false,
+	    dotsEach:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:2
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+    });
+    
+    $('.recommended-box-slider .inner-img-slider').owlCarousel({
+	    //loop:true,
+	    margin:17,
+	    dots: true,
+	    nav:false,
+	    dotsEach:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+    });
+    
+    $('.discover-img-slider').owlCarousel({
+	    //loop:true,
+	    margin:20,
+	    dots: true,
+	    nav:false,
+	    dotsEach:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:2
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+    })
+
+    
+    /*toggle menu*/
+    function openNav() {
+        document.getElementById("mySidepanel").style.width = "250px";
+    }
+    
+    function closeNav() {
+        document.getElementById("mySidepanel").style.width = "0";
+    }
+    /*toggle menu end*/
+    
+    
+});
