7cadbf1e5f1f377145ca4ac973796ce83309f560
starahmad6161 <starahmad616161@gmail.com>
2020-12-21 01:23:31 +0200

    fix slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,7 @@
-	$('#recommended-box-slider').owlCarousel({
-	    loop:true,
+$(function() {
+    'use strict';
+    $('.recommended-box-slider').owlCarousel({
+	    //loop:true,
 	    margin:25,
 	    dots: true,
 	    nav:false,
@@ -15,31 +17,10 @@
 	            items:3
 	        }
 	    }
-	})
-
-	$('#recommended-box-slider1').owlCarousel({
-		loop:true,
-		margin:25,
-		dots: true,
-		nav:false,
-		dotsEach:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:2
-			},
-			1000:{
-				items:3
-			}
-		}
-	})
- /*Recommended For You start sliderr over*/
-
- /*Recommended box-img-slider*/
- $('#recommended-img-slider-1').owlCarousel({
-	    loop:true,
+    });
+    
+    $('.recommended-box-slider .inner-img-slider').owlCarousel({
+	    //loop:true,
 	    margin:17,
 	    dots: true,
 	    nav:false,
@@ -55,219 +36,9 @@
 	            items:1
 	        }
 	    }
-	})
- $('#recommended-img-slider-2').owlCarousel({
-	    loop:true,
-	    margin:17,
-	    dots: true,
-	    nav:false,
-	    dotsEach:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:1
-	        }
-	    }
-	})
- $('#recommended-img-slider-3').owlCarousel({
-	    loop:true,
-	    margin:17,
-	    dots: true,
-	    nav:false,
-	    dotsEach:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:1
-	        }
-	    }
-	})
- $('#recommended-img-slider-4').owlCarousel({
-	    loop:true,
-	    margin:17,
-	    dots: true,
-	    nav:false,
-	    dotsEach:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:1
-	        }
-	    }
-	})
- $('#recommended-img-slider-5').owlCarousel({
-	    loop:true,
-	    margin:17,
-	    dots: true,
-	    nav:false,
-	    dotsEach:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:1
-	        }
-	    }
-	})
- $('#recommended-img-slider-6').owlCarousel({
-	    loop:true,
-	    margin:17,
-	    dots: true,
-	    nav:false,
-	    dotsEach:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:1
-	        }
-	    }
-	})
-
-	$('#recommended-img-slider-7').owlCarousel({
-		loop:true,
-		margin:17,
-		dots: true,
-		nav:false,
-		dotsEach:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:1
-			},
-			1000:{
-				items:1
-			}
-		}
-	})
-
-	$('#recommended-img-slider-8').owlCarousel({
-		loop:true,
-		margin:17,
-		dots: true,
-		nav:false,
-		dotsEach:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:1
-			},
-			1000:{
-				items:1
-			}
-		}
-	})
-
-	$('#recommended-img-slider-9').owlCarousel({
-		loop:true,
-		margin:17,
-		dots: true,
-		nav:false,
-		dotsEach:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:1
-			},
-			1000:{
-				items:1
-			}
-		}
-	})
-
-	$('#recommended-img-slider-10').owlCarousel({
-		loop:true,
-		margin:17,
-		dots: true,
-		nav:false,
-		dotsEach:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:1
-			},
-			1000:{
-				items:1
-			}
-		}
-	})
-
-	$('#recommended-img-slider-11').owlCarousel({
-		loop:true,
-		margin:17,
-		dots: true,
-		nav:false,
-		dotsEach:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:1
-			},
-			1000:{
-				items:1
-			}
-		}
-	})
-
-	$('#recommended-img-slider-12').owlCarousel({
-		loop:true,
-		margin:17,
-		dots: true,
-		nav:false,
-		dotsEach:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:1
-			},
-			1000:{
-				items:1
-			}
-		}
-	})
-
-
-
-
-	/*end*/
-
- /*discover start*/
- $('#discover-img-slider-1').owlCarousel({
+    });
+    
+    $('.discover-img-slider').owlCarousel({
 	    //loop:true,
 	    margin:20,
 	    dots: true,
@@ -284,15 +55,18 @@
 	            items:5
 	        }
 	    }
-	})
+    })
 
-
-/*toggle menu*/
-function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
-}
-/*toggle menu end*/
+    
+    /*toggle menu*/
+    function openNav() {
+        document.getElementById("mySidepanel").style.width = "250px";
+    }
+    
+    function closeNav() {
+        document.getElementById("mySidepanel").style.width = "0";
+    }
+    /*toggle menu end*/
+    
+    
+});
